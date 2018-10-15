02c14819c822b2ffb0fc746fe07324d63c870115
Steve Seguin <steve@studio3d.tv>
2018-10-15 19:40:01 -0400

    Removed old password

--- install_redis.sh
+++ install_redis.sh
@@ -1,3 +1,3 @@
 apt-get install redis -y && \
 apt-get install redis-tools -y && \
-redis-cli -h portal1369-16.bmix-dal-yp-cbfabb84-69f1-472d-87d7-343fd70e44c6.steve-seguin-email.composedb.com -p 40299 -a admin  config set notify-keyspace-events KEA
+redis-cli -h REDISSERVERURLHERE -p PASSWORDHERE -a admin  config set notify-keyspace-events KEA
