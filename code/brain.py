21e1d610fd88d03367790220d8fb1fdd6f424a4d
Steve Seguin <punkrawker@gmail.com>
2018-10-29 14:34:12 -0400

    remove object detector
    Just simplifies the code base.

--- code/brain.py
+++ code/brain.py
@@ -46,53 +46,6 @@ r = redis.Redis.from_url(config.RABBITMQ_CONFIG['uri'])
 #logging.basicConfig(level=logging.DEBUG)
 def tensorThread(something):
 
-	class NodeLookup():
-		def __init__(self, label_lookup_path=None, uid_lookup_path=None):
-			if not label_lookup_path:
-				label_lookup_path = 'inception/imagenet_2012_challenge_label_map_proto.pbtxt'
-			if not uid_lookup_path:
-				uid_lookup_path = 'inception/imagenet_synset_to_human_label_map.txt'
-			self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
-
-		def load(self, label_lookup_path, uid_lookup_path):
-			if not tf.gfile.Exists(uid_lookup_path):
-				tf.logging.fatal('File does not exist %s', uid_lookup_path)
-			if not tf.gfile.Exists(label_lookup_path):
-				tf.logging.fatal('File does not exist %s', label_lookup_path)
-			# Loads mapping from string UID to human-readable string
-			proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
-			uid_to_human = {}
-			p = re.compile(r'[n\d]*[ \S,]*')
-			for line in proto_as_ascii_lines:
-				parsed_items = p.findall(line)
-				uid = parsed_items[0]
-				human_string = parsed_items[2]
-				uid_to_human[uid] = human_string
-			#return uid_to_human
-			# Loads mapping from string UID to integer node ID.
-			node_id_to_uid = {}
-			proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
-			for line in proto_as_ascii:
-				if line.startswith('	target_class:'):
-					target_class = int(line.split(': ')[1])
-				if line.startswith('	target_class_string:'):
-					target_class_string = line.split(': ')[1]
-					node_id_to_uid[target_class] = target_class_string[1:-2]
-			# Loads the final mapping of integer node ID to human-readable string
-			node_id_to_name = {}
-			for key, val in node_id_to_uid.items():
-				if val not in uid_to_human:
-					tf.logging.fatal('Failed to locate: %s', val)
-				name = uid_to_human[val]
-				node_id_to_name[key] = name
-			return node_id_to_name
-			
-			
-	def create_graph():
-		with tf.gfile.FastGFile("inception/classify_image_graph_def.pb", 'rb') as f:
-			graph_def = tf.GraphDef()
-			graph_def.ParseFromString(f.read())
-			_ = tf.import_graph_def(graph_def, name='')
 
 
 	def url_to_image(url):
@@ -130,22 +83,7 @@ def tensorThread(something):
 		childChannel.stop_consuming()
 		msg = msg0.body
 		msg = json.loads(msg)
-		if (msg['task']=="ObjectOn"):
-			#img = url_to_image(msg['url'])
-
-			image = np.asarray(r.get(msg['redisID']), dtype="uint8")
-			image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-			
-			if type(img) == type(None):
-				print("no image on redis")
-				return
-
-			if type(img) == type(False):
-				print("couldnt process")
-				return
-			objectDetection(img, msg['streamKey'])
-		elif (msg['task']=="TrumpOn"):
+		if (msg['task']=="TrumpOn"):
 			try:	
 				image = np.asarray(bytearray(r.get(msg['redisID'])), dtype="uint8")
 			except:
@@ -295,21 +233,7 @@ def tensorThread(something):
 #					sendMessage("https://api.ooblex.com/images/"+filename, token)
 
 				#print(predictions)
-	def objectDetection(img, token):
-		predictions = sess_thing.run(softmax_tensor, {'DecodeJpeg:0': img})
-		predictions = np.squeeze(predictions)
-		return
-		node_lookup = NodeLookup()
-		top_k = predictions.argsort()[-1:][::-1]  ## top 3 predictions
-		for node_id in top_k:
-			human_string = str(node_lookup.node_lookup[str(node_id)])
-			score = predictions[node_id]
-			print('%s (score = %.5f)' % (human_string, score))
-			output = '%s (score = %.5f)' % (human_string, score)
-			sendMessage("Object Detected: "+output, token)
-
-
-
+	
 
 
 
