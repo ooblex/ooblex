3bdb19b8dd1bd53845aecb3a7a973c26e3b2d3c2
Steve Seguin <steve@seguin.email>
2019-05-06 23:39:51 -0400

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,5 +1,5 @@
 REDIS_CONFIG = {
-    'uri' : "rediss://admin:GSURJCGDVFWYOLYD@portal1369-16.bmix-dal-yp-cbfabb84-69f1-472d-87d7-343fd70e44c6.steve-seguin-email.composedb.com:40299"
+    'uri' : "redis://localhost"
 }
 
 RABBITMQ_CONFIG = {
