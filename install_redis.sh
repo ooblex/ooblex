c89d80594fc9e195c4920360d96c659c514422be
Steve Seguin <steve@seguin.email>
2019-05-06 23:03:39 -0400

    Update install_redis.sh
    no longer hosting redis myself for everyone, so just changing it to ensure people know that it is locally hosted now

--- install_redis.sh
+++ install_redis.sh
@@ -1,4 +1,9 @@
+
 apt-get install redis -y && \
 apt-get install redis-tools -y && \
-pip3 install redis && \
-redis-cli -h portal1369-16.bmix-dal-yp-cbfabb84-69f1-472d-87d7-343fd70e44c6.steve-seguin-email.composedb.com -p 40299 -a admin  config set notify-keyspace-events KEA
+pip3 install redis 
+redis-cli -h localhost
+
+
+## To test to see if your remotely hosted REDIS server is installed instead, if cloud hosted for example for use in a cluster...
+# redis-cli -h portal1369-16.bmix-dal-yp-cbfabb84-69f1-472d-87d7-343fd70e44c6.steve-seguin-email.composedb.com -p 40299 -a admin  config set notify-keyspace-events KEA
