f974894d8ec0355009615cfab5f654f108862288
Steve Seguin <steve@seguin.email>
2020-09-11 06:46:33 -0400

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,6 +1,6 @@
+sudo apt-get update
 apt-get install nginx -y
 
-sudo apt-get update
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
